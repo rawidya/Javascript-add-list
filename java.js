5eed37aba4e6f3d3cc25aceedb53f39f577575a5
Parama Widyadhana <paramawidyaputraarif@gmail.com>
2025-08-25 11:48:26 +0700

    new edit and delete

--- java.js
+++ java.js
@@ -4,35 +4,62 @@ const list = document.getElementById("list");
 
 button.addEventListener("click", function() {
     const teksv = teks.value.trim();
-    
+
     if (teksv !== "") {
         const cardNote = document.createElement("div");
         cardNote.className = "card-note";
 
         const spanText = document.createElement("span");
-        const divButton = document.createElement("div");
         spanText.textContent = teksv;
         cardNote.appendChild(spanText);
 
+        const divButton = document.createElement("div");
+
         const buttonEdit = document.createElement("button");
         buttonEdit.textContent = "Edit";
-
         buttonEdit.style.backgroundColor = "blue";
         divButton.appendChild(buttonEdit);
 
         const buttonDelete = document.createElement("button");
         buttonDelete.textContent = "Delete";
         buttonDelete.style.backgroundColor = "red";
+        divButton.appendChild(buttonDelete);
 
         cardNote.style.display = "flex";
         cardNote.style.justifyContent = "space-between";
-        divButton.appendChild(buttonDelete);
-
         cardNote.appendChild(divButton);
 
         list.appendChild(cardNote);
 
         teks.value = "";
+
+        // Edit
+        buttonEdit.addEventListener("click", function() {
+            const inputEdit = document.createElement("input");
+            inputEdit.type = "text";
+            inputEdit.value = spanText.textContent;
+            cardNote.insertBefore(inputEdit, spanText);
+            cardNote.removeChild(spanText);
+
+            buttonEdit.textContent = "Update list";
+            buttonEdit.style.backgroundColor = "green";
+
+            buttonEdit.onclick = function() {
+                spanText.textContent = inputEdit.value;
+                cardNote.insertBefore(spanText, inputEdit);
+                cardNote.removeChild(inputEdit);
+
+                buttonEdit.textContent = "Edit";
+                buttonEdit.style.backgroundColor = "blue";
+                buttonEdit.onclick = null;
+                buttonEdit.addEventListener("click", arguments.callee);
+                
+            };
+        });
+        // Delete
+         buttonDelete.addEventListener("click", function() {
+            cardNote.remove();});
+
     } else {
         alert("Please enter a task");
     }
